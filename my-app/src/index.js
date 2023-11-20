52d766d7699320870d32bded7ed1c34ab6aa478e
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-11-19 22:42:16 -0600

    changed square from class to function
    function is a data structure specifically for classes that only have a render function
    
    improves efficiency
    
    shouldComponentUpdate()

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+function Square(props) {
         return (
             // call board handleClick(i) method from square
             <button
                 className="square"
-                onClick={() => this.props.onClick()}
+                onClick={() => props.onClick()}
             >
-                {this.props.value}
+                {props.value}
             </button>
         );
-    }
 }
 
 class Board extends React.Component {
