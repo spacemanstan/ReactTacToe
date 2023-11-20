90c66e159f882e75fc8599c61eba7da937cb4724
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-11-19 23:21:00 -0600

    Taking turns - resolves #2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -20,13 +20,17 @@ class Board extends React.Component {
         // initialize an empty board of 9 squares
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -40,7 +44,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
@@ -85,3 +89,26 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function calculateWinner(squares) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    for (let index = 0; index < lines.length; ++i) {
+      const [a, b, c] = lines[i];
+      
+      if(squares[a] && squares[b] === squares [b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+}
